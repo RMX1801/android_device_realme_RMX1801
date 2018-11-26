bae3ca2d183cf3f37d3368b6fc189ce8dc7b017d
Sumit Das <sumidas@codeaurora.org>
2018-11-26 15:01:19 -0800

    RMX1801: IMS: Remove hidden API references in ims.apk
    Add telephony-qti-utils libarary to product packages
    
    Change-Id: Ie8651ebadc246c21bd5eb69ef060fe32d8f5f7c8

--- device.mk
+++ device.mk
@@ -343,7 +343,9 @@ PRODUCT_PACKAGES += \
     librmnetctl \
     libxml2  \
     qti-telephony-hidl-wrapper \
-    qti_telephony_hidl_wrapper.xml
+    qti_telephony_hidl_wrapper.xml \
+    qti-telephony-utils \
+    qti_telephony_utils.xml
 
 # RenderScript
 PRODUCT_PACKAGES += \
