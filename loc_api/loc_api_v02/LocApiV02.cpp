88ad0874936aec71d9eca63ad71d30f393fd5c31
Mike Cailean <mcailean@codeaurora.org>
2019-03-12 16:49:20 -0700

    Discontinuity clock is wrongly updated for multiband
    HW discontinuity clock is wrongly update for
    multiband for clock conversion is done twice for
    multiband (one for L1 and one for L5)
    
    Change-Id: I35e0f9a61e75b793cc54346c0eca4e8db44e86c8
    CRs-fixed: 2414386

--- loc_api/loc_api_v02/LocApiV02.cpp
+++ loc_api/loc_api_v02/LocApiV02.cpp
@@ -4917,7 +4917,8 @@ void LocApiV02 :: reportGnssMeasurementData(
                  measurementsNotify.count);
     }
     // the GPS clock time reading
-    if (eQMI_LOC_SV_SYSTEM_GPS_V02 == gnss_measurement_report_ptr.system) {
+    if (eQMI_LOC_SV_SYSTEM_GPS_V02 == gnss_measurement_report_ptr.system &&
+        false == bGPSreceived) {
         bGPSreceived = true;
         msInWeek = convertGnssClock(measurementsNotify.clock,
                                     gnss_measurement_report_ptr);
