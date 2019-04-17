df1330033abf9f16949fb2c2fec98502533467aa
GuaiYiHu <guaiyihu@foxmail.com>
2019-04-17 16:57:34 +0800

    RMX1801: Build broadcastradio hidl impl
    Signed-off-by: ShubhamB <shubhamprince111@gmail.com>
    
    Change-Id: I206755bf3f3a34337c48608156043d6849c67008

--- device.mk
+++ device.mk
@@ -131,7 +131,8 @@ PRODUCT_PACKAGES += \
 
 # Configstore
 RODUCT_PACKAGES += \
-    android.hardware.configstore@1.1-service
+    android.hardware.configstore@1.1-service \
+    android.hardware.broadcastradio@1.0-impl
 
 # Display
 PRODUCT_PACKAGES += \
