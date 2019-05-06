5f3cf2aac6ca41cf163628c1990d2fbe1d1e0444
codeworkx <daniel.hillenbrand@codeworkx.de>
2019-05-06 20:01:51 +0200

    RMX1801: build libstdc++.vendor for camera hal
    Change-Id: I25c11cb5545fb38902eab322d860b3374be6f9ce

--- device.mk
+++ device.mk
@@ -129,6 +129,7 @@ PRODUCT_PACKAGES += \
     android.frameworks.displayservice@1.0 \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
+    libstdc++.vendor \
     Snap
 
 # Configstore
