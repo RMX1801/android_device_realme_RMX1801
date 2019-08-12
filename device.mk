8816cc6dacb97e2ee799af7ecefcd35a19b204e6
AmulyaX <amulya.b520@gmail.com>
2019-08-12 18:44:35 +0530

    RMX1801: Import Lights HAL
    - Import the HAL from LineageOS/android_device_xiaomi_msm8998-common &
    - Relevant changes in selinux file_contexts.
    - This includes the recent fixes from upstream and makes
      it much cleaner. Note, I removed the chown/chmods because
      they are already done in init.rc/ueventd.rc/init.qcom.rc.
    
    [@sb6596 edits: s/red/white]
    
    Co-authored-by: Michael Bestas <mkbestas@lineageos.org>
    Co-authored-by: Wang Han <416810799@qq.com>
    Co-authored-by: Atman <masteratman@gmail.com>
    Change-Id: I0b551bcea366a70044760c426832b5dba2cffa51

--- device.mk
+++ device.mk
@@ -87,6 +87,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/keylayout/gpio-keys.kl:system/usr/keylayout/gpio-keys.kl
 
+# Lights
+PRODUCT_PACKAGES += \
+    android.hardware.light@2.0-service.realme_sdm660
+
 # Media
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media_profiles_vendor.xml:system/etc/media_profiles_vendor.xml
