39e07a05f719334495af10d4ae66e9c550591053
Davide Garberi <dade.garberi@gmail.com>
2019-10-02 19:33:08 +0200

    RMX1801: Switch to power 1.2
    Change-Id: I7eba0105280192e1f397dafed7c0c3211435600e

--- device.mk
+++ device.mk
@@ -278,7 +278,7 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.1-service-qti
+    android.hardware.power@1.2-service-qti
 
 # Public Libraries
 PRODUCT_COPY_FILES += \
