a3732893c232522d093f1b0561de26e84745b216
sb6596 <shubhamprince111@gmail.com>
2019-10-24 13:47:10 +0000

    RMX1801: Initial base tree
    Change-Id: I854e63620d7d9bb801caa20795f7350e30a4c3e8

---
+++ device.mk
@@ -0,0 +1,53 @@
+#
+# Copyright (C) 2018 The Android Open-Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Get non-open-source specific aspects
+$(call inherit-product, vendor/realme/RMX1801/RMX1801-vendor.mk)
+
+# Overlays
+DEVICE_PACKAGE_OVERLAYS += \
+    $(LOCAL_PATH)/overlay
+
+# AAPT
+PRODUCT_AAPT_CONFIG := normal
+PRODUCT_AAPT_PREF_CONFIG := xxhdpi
+
+# Asserts
+TARGET_OTA_ASSERT_DEVICE := RMX1801
+
+# Boot Animation
+TARGET_SCREEN_HEIGHT := 2048
+TARGET_SCREEN_WIDTH := 1080
+
+# Audio
+PRODUCT_PACKAGES += \
+    audio.a2dp.default
+
+# IMS
+PRODUCT_PACKAGES += \
+    ims-ext-common
+
+# Init
+PRODUCT_PACKAGES += \
+    init.qcom.rc \
+    fstab.qcom
+
+# Telephony-ext
+PRODUCT_PACKAGES += \
+    telephony-ext
+
+PRODUCT_BOOT_JARS += \
+    telephony-ext
---
+++ lineage_RMX1801.mk
@@ -0,0 +1,46 @@
+#
+# Copyright (C) 2019 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Inherit some common AOSP stuff
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common Lineage stuff
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+# Inherit from Realme RMX1801
+$(call inherit-product, device/realme/RMX1801/device.mk)
+
+# Set Shipping API level
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
+
+PRODUCT_NAME := lineage_RMX1801
+PRODUCT_DEVICE := RMX1801
+PRODUCT_MANUFACTURER := OPPO
+PRODUCT_BRAND := oppo
+PRODUCT_MODEL := Realme 2 Pro
+
+PRODUCT_GMS_CLIENTID_BASE := android-oppo
+
+TARGET_VENDOR_PRODUCT_NAME := RMX1801
+TARGET_VENDOR_DEVICE_NAME := RMX1801
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    TARGET_DEVICE="RMX1801" \
+    PRODUCT_NAME="RMX1801" \
+    PRIVATE_BUILD_DESC="sdm660_64-user 9 PKQ1.181121.001 eng.root.20190824.025904 release-keys"
+
+BUILD_FINGERPRINT := "OPPO/RMX1801/RMX1801:9/PKQ1.181121.001/1566224186:user/release-keys"
