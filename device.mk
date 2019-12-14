08c1175ed3b38590bdf49cd6f8b70517b15c1e01
Muhammed Siju <msiju@codeaurora.org>
2019-12-14 09:36:27 +0000

    RMX1801: Telephony: Up rev radio hal versions
    IRadio to 1.4
    IRadioConfig to 1.1
    IImsRadio to 1.5
    
    Change-Id: Icdac83eb3268d3323532d046837baa42eb364dd3

--- device.mk
+++ device.mk
@@ -336,7 +336,9 @@ PRODUCT_PACKAGES += \
 
 # Radio
 PRODUCT_PACKAGES += \
+    android.hardware.radio@1.4 \
     android.hardware.radio@1.2 \
+    android.hardware.radio.config@1.1 \
     android.hardware.radio.config@1.0 \
     android.hardware.secure_element@1.0 \
     libprotobuf-cpp-full \
