9ecca4535ae071a71af604a11d562629f2b30535
ShubhamB <shubhamprince111@gmail.com>
2019-12-27 15:35:58 +0000

    RMX1801: Use TARGET_SYSTEM_PROP for system properties
    Change-Id: I6d7749a4cd00b9f3487139efcb47ba88dc0566a1

--- device.mk
+++ device.mk
@@ -57,7 +57,6 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/handheld_core_hardware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/handheld_core_hardware.xml
 
 # Properties
--include $(LOCAL_PATH)/system_prop.mk
 -include $(LOCAL_PATH)/vendor_prop.mk
 
 # AAPT
