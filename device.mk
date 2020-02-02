4ac27b659d46ed57658f36c8795593e828fef581
dianlujitao <dianlujitao@lineageos.org>
2020-02-02 19:33:37 +0800

    RMX1801: Explictly build camera HIDL interfaces
     * The 64-bit variants are used by IMS blobs but no longer automatically
       built along with the 32-bit counterparts.
    
    Change-Id: I387e4345c3088bbaf55e24bcfe286ccd877507a6

--- device.mk
+++ device.mk
@@ -159,10 +159,13 @@ PRODUCT_PACKAGES += \
 # Camera
 PRODUCT_PACKAGES += \
     android.frameworks.displayservice@1.0 \
+    android.hardware.camera.device@3.4 \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
+    android.hardware.camera.provider@2.5 \
     libstdc++.vendor \
-    Snap
+    Snap \
+    vendor.qti.hardware.camera.device@1.0
 
 # Configstore
 RODUCT_PACKAGES += \
