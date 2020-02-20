b10f5c342dcb64972e5b2a0a64dcc7c608b1d311
ShubhamB <shubhamprince111@gmail.com>
2020-02-20 13:41:50 +0000

    RMX1801: Build libsensorndkbridge
    Change-Id: I53074a08007ef2e6e2545c810f5ca6d1d060ad15

--- device.mk
+++ device.mk
@@ -306,7 +306,8 @@ PRODUCT_PACKAGES += \
 # Sensors
 PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl \
-    android.hardware.sensors@1.0-service
+    android.hardware.sensors@1.0-service \
+    libsensorndkbridge
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
