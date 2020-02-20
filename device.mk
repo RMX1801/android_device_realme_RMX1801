8a6cccd653d9ba09f23ad9c702078bc5385212e6
ShubhamB <shubhamprince111@gmail.com>
2020-02-20 13:42:37 +0000

    RMX1801: Build thermal HAL from source
    Change-Id: I3a2dfca7df859546faf355b46ecb21c53f72cdd3

--- device.mk
+++ device.mk
@@ -350,7 +350,8 @@ PRODUCT_PACKAGES += \
 # Thermal
 PRODUCT_PACKAGES += \
     android.hardware.thermal@1.0-impl \
-    android.hardware.thermal@1.0-service
+    android.hardware.thermal@1.0-service \
+    thermal.sdm660
 
 # Trust HAL
 PRODUCT_PACKAGES += \
