2183abc31a5c4af73e5dde1a5139da0da6664cdf
ShubhamB <shubhamprince111@gmail.com>
2020-05-11 17:11:17 +0530

    RMX1801: Re-add touch HAL
    Signed-off-by: ShubhamB <shubhamprince111@gmail.com>

--- device.mk
+++ device.mk
@@ -421,6 +421,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf
 
+# Touch
+PRODUCT_PACKAGES += \
+    vendor.lineage.touch@1.0-service.realme_sdm660
+
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service.basic
