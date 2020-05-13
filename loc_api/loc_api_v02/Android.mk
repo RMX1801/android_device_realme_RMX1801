9dc0c198fd02e8d1eb6d2f84f68c64ba1506fae6
Vamana Murthi <vthuniki@codeaurora.org>
2020-05-13 17:02:27 +0530

    add LOCAL_C_INCLUDES for qmi header library
    LOCAL_C_INCLUDES for qmi header is needed to
    fix HY22 build error
    
    Change-Id: I7d4cc321bcfb83ad59c00c0ebbf68bbdcc09de8a
    CRs-Fixed: 2685294

--- loc_api/loc_api_v02/Android.mk
+++ loc_api/loc_api_v02/Android.mk
@@ -32,6 +32,9 @@ LOCAL_CFLAGS += \
     -D_ANDROID_
 
 ## Includes
+LOCAL_C_INCLUDES := \
+    $(TARGET_OUT_HEADERS)/qmi-framework/inc \
+    $(TARGET_OUT_HEADERS)/qmi/inc
 LOCAL_HEADER_LIBRARIES := \
     libloc_core_headers \
     libgps.utils_headers \
