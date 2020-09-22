ede9b06a9895ef63576d52448636d76c7eff3b26
Wei (GPS SD) Chen <weic@codeaurora.org>
2020-09-22 14:40:09 -0700

    Location hal daemon: free the memory of gnss update/get config
    The memory returned by gnss update/get config need to be freed
    
    Change-Id: I2e8595ff07fd0838fdd3323d6539ccf73ce94483
    CRs-fixed: 2782920

--- location_hal_daemon/LocationApiService.h
+++ location_hal_daemon/LocationApiService.h
@@ -171,10 +171,15 @@ private:
     void pingTest(LocAPIPingTestReqMsg*);
 
     inline uint32_t gnssUpdateConfig(const GnssConfig& config) {
-        uint32_t* sessioIds =  mLocationControlApi->gnssUpdateConfig(config);
+        uint32_t* sessionIds =  mLocationControlApi->gnssUpdateConfig(config);
         // in our usage, we only configure one setting at a time,
         // so we have only one sessionId
-        return *sessioIds;
+        uint32_t sessionId = 0;
+        if (sessionIds) {
+            sessionId = *sessionIds;
+            delete [] sessionIds;
+        }
+        return sessionId;
     }
 
     // Location control API callback
