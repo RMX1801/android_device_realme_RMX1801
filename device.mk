019015449a5e4aac964061dde3f8870587df832e
ShubhamB <shubhamprince111@gmail.com>
2020-10-28 20:33:25 +0530

    RMX1801: Build btconfigstore from source
    Signed-off-by: ShubhamB <shubhamprince111@gmail.com>

--- device.mk
+++ device.mk
@@ -147,7 +147,8 @@ PRODUCT_PACKAGES += \
     android.hardware.bluetooth.audio@2.0-impl \
     audio.bluetooth.default \
     libbluetooth_audio_session \
-    libbthost_if
+    libbthost_if \
+    vendor.qti.hardware.btconfigstore@1.0.vendor
 
 # Camera
 PRODUCT_PACKAGES += \
