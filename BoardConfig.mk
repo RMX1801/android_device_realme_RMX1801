66fd9c1f69123b95710768a050d48a945f8519a5
ShubhamB <shubhamprince111@gmail.com>
2020-11-14 12:38:50 +0530

    RMX1801: Add mkbootimg args for offsets
     * As BOARD_RAMDISK_OFFSET & BOARD_KERNEL_TAGS_OFFSET are deprecated now
    
    Signed-off-by: ShubhamB <shubhamprince111@gmail.com>
    Change-Id: I8f3026d6f60427446e97c9ce2c88011da70cef43

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -49,9 +49,14 @@ TARGET_USES_64_BIT_BINDER := true
 BOARD_KERNEL_CMDLINE := console=ttyMSM0,115200,n8 androidboot.console=ttyMSM0 earlycon=msm_serial_dm,0xc170000 androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x37 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 sched_enable_hmp=1 sched_enable_power_aware=1 service_locator.enable=1 swiotlb=1 loop.max_part=7
 BOARD_KERNEL_CMDLINE += usbcore.autosuspend=7
 BOARD_KERNEL_BASE := 0x00000000
-BOARD_KERNEL_TAGS_OFFSET := 0x00000100
-BOARD_RAMDISK_OFFSET     := 0x01000000
 BOARD_PAGE_SIZE := 4096
+BOARD_RAMDISK_OFFSET     := 0x01000000
+BOARD_SECOND_OFFSET := 0x00f00000
+BOARD_TAGS_OFFSET := 0x00000100
+BOARD_MKBOOTIMG_ARGS += --ramdisk_offset $(BOARD_RAMDISK_OFFSET)
+BOARD_MKBOOTIMG_ARGS += --second_offset $(BOARD_SECOND_OFFSET)
+BOARD_MKBOOTIMG_ARGS += --tags_offset $(BOARD_TAGS_OFFSET)
+BOARD_MKBOOTIMG_ARGS += --pagesize $(BOARD_PAGE_SIZE)
 TARGET_KERNEL_ARCH := arm64
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 TARGET_KERNEL_CLANG_COMPILE := true
