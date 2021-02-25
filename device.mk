e07e5f51cc609989e23751333597203264531b2f
Subhajeet Muhuri <subhajeet.muhuri@aosip.dev>
2021-02-25 20:31:33 +0530

    RMX1801: power-libperfmgr: Switch to AIDL Power HAL hardware/google/pixel/power-libperfmgr from android-11.0.0_r25
    SQUASHED:
    Revert all HIDL interface and nuke previous changes
    Import Pixel libperfmgr AIDL Power HAL
    Adapt and rebrand for realme_sdm660
    Remove Google-specific display LPM control
    Remove Google-specific camera and audio hints
    Remove VR hints handling
    Remove audio hints handling
    Remove dumpstate support
    Initialize powerHAL when boot is completed
    Add support for tap-to-wake feature control
    Add sepolicy rules for power-libperfmgr
    Enable power-libperfmgr
    
    Signed-off-by: Subhajeet Muhuri <subhajeet.muhuri@aosip.dev>

--- device.mk
+++ device.mk
@@ -320,7 +320,7 @@ PRODUCT_COPY_FILES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.3-service.realme_sdm660-libperfmgr
+    android.hardware.power-service.realme_sdm660-libperfmgr
 
 # Powerhint
 PRODUCT_COPY_FILES += \
