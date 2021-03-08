09e07c62cea4c38445450d27a44c5efb2718d4dc
Michael Bestas <mkbestas@lineageos.org>
2021-03-07 23:14:21 -0300

    RMX1801: sepolicy: Switch to SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS
     Fixes: warning: BOARD_PLAT_PRIVATE_SEPOLICY_DIR has been deprecated. Use
     SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS instead. warning:
    
    Change-Id: I4ab24dba858db1b0bcd7010bdf7a19f607bdd026

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -188,7 +188,7 @@ TARGET_COPY_OUT_VENDOR := vendor
 
 # Sepolicy
 include device/qcom/sepolicy-legacy-um/SEPolicy.mk
-BOARD_PLAT_PRIVATE_SEPOLICY_DIR += $(DEVICE_PATH)/sepolicy/private
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 
 # WiFi
