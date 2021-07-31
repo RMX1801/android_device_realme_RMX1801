a6d8e4f1a39dded5020fac3e12ed8286e4d4baaa
sb6596 <shubhamprince111@gmail.com>
2021-07-31 03:44:42 +0000

    RMX1801: Build mock power.stats HAL
    Signed-off-by: sb6596 <shubhamprince111@gmail.com>

--- device.mk
+++ device.mk
@@ -327,7 +327,8 @@ PRODUCT_COPY_FILES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service.realme_sdm660-libperfmgr
+    android.hardware.power-service.realme_sdm660-libperfmgr \
+    android.hardware.power.stats@1.0-service.mock
 
 # Powerhint
 PRODUCT_COPY_FILES += \
