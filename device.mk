b6cb0b03ebae772d38f538c729e78bb6e773114c
sb6596 <shubhamprince111@gmail.com>
2021-08-17 09:06:11 +0000

    RMX1801: Track IRQ config from device tree
    Signed-off-by: sb6596 <shubhamprince111@gmail.com>

--- device.mk
+++ device.mk
@@ -266,6 +266,10 @@ PRODUCT_PACKAGES += \
     init.target.rc \
     ueventd.qcom.rc
 
+# IRQ Config
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/msm_irqbalance.conf:$(TARGET_COPY_OUT_VENDOR)/etc/msm_irqbalance.conf
+
 # IRSC
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sec_config:$(TARGET_COPY_OUT_VENDOR)/etc/sec_config
