52658ca28757685acd3832f659c93b6f454673d2
orges <me@orgesified.ml>
2021-10-13 16:39:40 +0000

    RMX1801: add display config file
     * DisplayPowerController[0]: Screen brightness nits configuration is unavailable; falling back
     * DisplayDeviceConfig: requesting nits when no mapping exists.
    
    Signed-off-by: orges <me@orgesified.ml>
    Change-Id: I1716319bb17b9236d07d44a2a7fa3c7640c54ddf

--- device.mk
+++ device.mk
@@ -225,6 +225,10 @@ PRODUCT_PACKAGES += \
     vendor.display.config@1.9 \
     vendor.display.config@1.9_vendor
 
+# Display Device Config
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/displayconfig/display_id_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/displayconfig/display_id_0.xml
+
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl:64 \
