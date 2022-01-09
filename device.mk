505114d53642d1d698e39bd61a8507340970ee1a
sb6596 <shubhamprince111@gmail.com>
2022-01-09 19:18:49 +0000

    Revert "RMX1801: Add dummy perf profile."
     * Since we have libperfmgr
    
    This reverts commit 7973bbd58568260e74aadce8ef4a0ce146deca54.

--- device.mk
+++ device.mk
@@ -390,9 +390,6 @@ PRODUCT_PACKAGES += \
     libqti-perfd-client \
     vendor.qti.hardware.perf@2.0.vendor
 
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/perf-profile0.conf:$(TARGET_COPY_OUT_VENDOR)/etc/perf/perf-profile0.conf
-
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power-service.realme_sdm660-libperfmgr \
