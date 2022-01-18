9fe24803dfb6d1678815a71ae14ec53f74620967
sb6596 <shubhamprince111@gmail.com>
2022-01-18 03:40:53 +0000

    RMX1801: Provide vintf_fragment for DRM-Widevine service
     * Also drop DRM @1.0 HALS while at it
    
    Signed-off-by: sb6596 <shubhamprince111@gmail.com>
    Change-Id: I9fe4f86e4cd7ccdc4a74382ab08369dc928c04ea

--- device.mk
+++ device.mk
@@ -237,8 +237,6 @@ PRODUCT_COPY_FILES += \
 
 # DRM
 PRODUCT_PACKAGES += \
-    android.hardware.drm@1.0-impl:64 \
-    android.hardware.drm@1.0-service \
     android.hardware.drm@1.0.vendor \
     android.hardware.drm@1.1.vendor \
     android.hardware.drm@1.2.vendor \
